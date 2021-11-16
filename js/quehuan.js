09df993c87425cbd26021207646fe7fb10aebd69
Guojiancode <1844718096@qq.com>
2021-11-16 13:54:49 +0800

    第四次商城作业

--- js/quehuan.js
+++ js/quehuan.js
@@ -19,4 +19,30 @@ $(function(){
 				controlsCurrentColor:"#ff6600",//当前控制按钮的颜色
 				isShowNum:true //是否显示数字
 			});
+			
+			/*图书 电子书小轮播*/
+						$('#ebooks-banner').tyslide({
+									boxh:223,//盒子的高度
+									w:332,//盒子的宽度
+									h:223,//图片的高度
+									isShow:true,//是否显示控制器
+									isShowBtn:true,//是否显示左右按钮
+									controltop:10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+									controlsW:20,//控制按钮宽度
+									controlsH:2,//控制按钮高度
+									controlsColor:"#d7d7d7",//普通控制按钮的颜色
+									controlsCurrentColor:"#00ff00",//当前控制按钮的颜色
+						});
+
+			
+			/*新书列表手风琴效果*/
+			$('.ebooks .right-box ul > li').mouseenter(function(){
+				//所有兄弟：隐藏详情 显示标题
+				$(this).sibligs().find('.desc').hide();
+				$(this).sibligs().find('.ebooks-title').show();
+				//当前：隐藏标题 显示详情
+				$(this).sibligs().find('.ebooks-title').hide();
+				$(this).sibligs().find('.desc').show();
+			})
+			
 })
